bab499a96f52b1b56f39a1f4cdc82b1fccf7d474
Flying <13223076623@163.com>
2025-10-18 14:30:50 +0800

    add ExtraParams

--- concurrency.go
+++ concurrency.go
@@ -1,6 +1,9 @@
 package goo
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 func ConcurrentWithLimit[A, B any](data []A, limit int, processFunc func(A) B) []B {
 	var back = make([]B, len(data))
@@ -22,4 +25,39 @@ func ConcurrentWithLimit[A, B any](data []A, limit int, processFunc func(A) B) [
 	wg.Wait()
 
 	return back
+}
+
+func ConcurrentWithLimitAndExtraParams[A, B any, EK comparable, EV any](data []A, limit int, extra map[EK]EV, processFunc func(A, map[EK]EV) (B, error)) ([]B, error) {
+	var (
+		wg sync.WaitGroup
+
+		errs error
+
+		back = make([]B, len(data))
+
+		semaphore = make(chan struct{}, limit)
+	)
+
+	for k, a := range data {
+		wg.Add(1)
+		semaphore <- struct{}{}
+		go func(ik int, ia A) {
+			defer func() {
+				<-semaphore
+				wg.Done()
+			}()
+			ret, err := processFunc(ia, extra)
+
+			if err != nil {
+				errs = errors.Join(errs, err)
+				return
+			}
+
+			back[ik] = ret
+		}(k, a)
+	}
+
+	wg.Wait()
+
+	return back, errs
 }
